9b07d89ef688a4790cb21c68341040644d8ed474
ParkJeongHo <pjh31208@naver.com>
2023-02-26 01:03:11 +0900

    Chore(install): Install Chakra UI and setting in index file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { worker } from './_mocks/worker';
+import { ChakraProvider } from '@chakra-ui/react';
 
 if (process.env.NODE_ENV === 'development') {
   worker.start();
@@ -12,8 +13,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ChakraProvider>
   </React.StrictMode>
 );
