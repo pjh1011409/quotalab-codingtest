ab1f9f58d10f976b33f7f9899aac5f729e4c6a6e
ParkJeongHo <pjh31208@naver.com>
2023-02-26 01:04:21 +0900

    Design(UI): Create topmenu in addpage

--- src/components/StakeholderAdd/TopMenu.tsx
+++ src/components/StakeholderAdd/TopMenu.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
 const TopMenu = () => {
-  return <div>TopMenu</div>;
+  return (
+    <>
+      <TopMenuWrapper>
+        <Title>주주 추가하기</Title>
+      </TopMenuWrapper>
+    </>
+  );
 };
 
 export default TopMenu;
+
+const TopMenuWrapper = styled.div`
+  width: full;
+  height: 70px;
+`;
+
+const Title = styled.div`
+  color: rgb(51, 51, 51);
+  text-overflow: ellipsis;
+  font-size: 18px;
+  font-family: 'Spoqa Han Sans Neo', sans-serif;
+  font-weight: 700;
+  padding: 24px 0px 20px 0px;
+  text-align: left;
+`;
