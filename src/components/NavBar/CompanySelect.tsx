a31284bd1d630a4c7336409f3e7a7daab479d678
ParkJeongHo <pjh31208@naver.com>
2023-02-26 00:54:19 +0900

    Design(UI): Create selector in navBar

--- src/components/NavBar/CompanySelect.tsx
+++ src/components/NavBar/CompanySelect.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { faBuilding } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Select } from 'antd';
 
 export const CompanySelect = () => {
-  return <div>CompanySelect</div>;
+  return (
+    <>
+      <BuildingLogo>
+        <FontAwesomeIcon icon={faBuilding} color="gray" />
+      </BuildingLogo>
+      <Select
+        style={{ margin: '0px 5px', width: '140px', textAlign: 'center' }}
+        defaultValue={'주식회사 벨코즈'}
+        options={[{ value: `1`, label: '주식회사 벨코즈' }]}
+      />
+    </>
+  );
 };
+
+const BuildingLogo = styled.div`
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  background-color: #f8f8f8;
+  border: 1px solid #dedfe1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0px 5px;
+`;
