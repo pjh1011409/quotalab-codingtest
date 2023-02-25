4bd99149e532085017eb19e240784cd94dc06134
ParkJeongHo <pjh31208@naver.com>
2023-02-26 00:53:40 +0900

    Design(UI): Create header in navbar

--- src/components/NavBar/QuotaHeader.tsx
+++ src/components/NavBar/QuotaHeader.tsx
@@ -1,5 +1,27 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import { css } from '@emotion/css';
+import { Link } from 'react-router-dom';
 
 export const QuotaHeader = () => {
-  return <div>QuotaHeader</div>;
+  return (
+    <QuotaHeaderWrapper>
+      <Link to="/">
+        <img
+          src="https://d1g8gmm07nz19n.cloudfront.net/logos/signature_horizon_black.png"
+          className={css`
+            height: 15px;
+            vertical-align: middle;
+          `}
+          alt="쿼타북 로고"
+        />
+      </Link>
+    </QuotaHeaderWrapper>
+  );
 };
+
+const QuotaHeaderWrapper = styled.div`
+  display: flex;
+  margin: 20px 5px 20px 20px;
+  width: 124px;
+`;
