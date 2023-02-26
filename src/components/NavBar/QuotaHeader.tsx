b77a5c1f692bb410c3c94bafc232dbed5ad4d599
ParkJeongHo <pjh31208@naver.com>
2023-02-26 16:52:16 +0900

    Refactor: Separate style components

--- src/components/NavBar/QuotaHeader.tsx
+++ src/components/NavBar/QuotaHeader.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { css } from '@emotion/css';
 import { Link } from 'react-router-dom';
 
-export const QuotaHeader = () => {
+const QuotaHeader = () => {
   return (
     <QuotaHeaderWrapper>
       <Link to="/">
-        <img
-          src="https://d1g8gmm07nz19n.cloudfront.net/logos/signature_horizon_black.png"
-          className={css`
-            height: 15px;
-            vertical-align: middle;
-          `}
-          alt="쿼타북 로고"
-        />
+        <Logo src="logo.png" alt="쿼타북 로고" />
       </Link>
     </QuotaHeaderWrapper>
   );
 };
 
+export default QuotaHeader;
+
 const QuotaHeaderWrapper = styled.div`
   display: flex;
   margin: 20px 5px 20px 20px;
   width: 124px;
+`;
+
+const Logo = styled.img`
+  width: 130px;
 `;
