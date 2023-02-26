9674e4d759b3137f0e65bf0a44703814af0b8ff1
ParkJeongHo <pjh31208@naver.com>
2023-02-26 17:28:40 +0900

    HotFix(ux): Add Toast UI when create new stakholder data

--- src/hooks/useAddStakeholder.ts
+++ src/hooks/useAddStakeholder.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useMutation, useQueryClient } from 'react-query';
 import { useNavigate } from 'react-router-dom';
 import { QueryKeys } from 'src/queryClient';
+import { useCustomToast } from './useCustomToast';
 
 interface MutableStakeholder {
   name: string;
@@ -23,6 +24,7 @@ const addStakeholderFetch = async ({ name, stockType, stockAmount }: MutableStak
 
 const useAddStakeholder = () => {
   const navigate = useNavigate();
+  const toast = useCustomToast();
   const queryClient = useQueryClient();
 
   const { mutate: createStakeholder } = useMutation(
@@ -31,6 +33,11 @@ const useAddStakeholder = () => {
       onSuccess: () => {
         queryClient.invalidateQueries(QueryKeys.stakeholder);
         navigate('/');
+        toast({
+          title: '주주정보가 추가되었습니다!',
+          status: 'success',
+          isClosable: true,
+        });
       },
     }
   );
